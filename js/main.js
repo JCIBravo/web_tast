565f177c6bfdfad0114e35c050b6916e2e77612f
Joan Illescas <juan.illescas.7e3@itb.cat>
2022-10-11 16:50:01 +0200

    Cookies

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-function main() {
-  alert("Cuadro de diálogo")
+var taskID = 0
+const tasksMade = []
+
+class Task {
+  constructor(taskID, taskName, taskDate, taskTime) {
+    this.taskID = taskID;
+    this.taskName = taskName;
+    this.taskDate = taskDate;
+    this.taskTime = taskTime;
+  }
 }
 
-var taskID = 0
-
+//program
 function showTaskCreator(){
   var divElement = document.getElementById("generarTasca");
   if (divElement.style.display === "none") {
@@ -24,10 +31,11 @@ function setTodayValue(){
   document.getElementById("tareaDate").value = today;
 }
 
-function addTask(){
-  date = document.getElementById("tareaDate") + document.getElementById("tareaTime")
-  Date.parse()
+function sendDataToPutTask(){
+  putTask(document.getElementById("tareaNombre").value, document.getElementById("tareaDate").value, document.getElementById("tareaTime").value)
+}
 
+function putTask(taskName, taskDate, taskTime){
   var navToAppend = document.getElementsByTagName("nav")[1];
   taskID++;
 
@@ -39,33 +47,38 @@ function addTask(){
   var div1 = document.createElement("div");
   div1.className = "items";
 
-  var input1 = document.createElement("input")
-  input1.className = "vertical-center"
-  input1.type = "image"
-  input1.src = "./img/botones/sincheck.png"
-  input1.name = "completado"
-  input1.alt = "Marcar como completado"
-  input1.height = "45"
-  input1.width = "45"
-  div1.appendChild(input1)
-  divPrincipal.appendChild(div1)
+  var input1 = document.createElement("input");
+  input1.className = "vertical-center";
+  input1.type = "image";
+  input1.src = "./img/botones/sincheck.png";
+  input1.name = "completado";
+  input1.alt = "Marcar como completado";
+  input1.height = "45";
+  input1.width = "45";
+  div1.appendChild(input1);
+  divPrincipal.appendChild(div1);
 
-  var div2 = document.createElement("div")
-  div2.className = "items"
+  var div2 = document.createElement("div");
+  div2.className = "items";
 
-  var p1 = document.createElement("p")
-  p1.className = "textitem"
-  p1.textContent = document.getElementById("tareaNombre").value
+  var p1 = document.createElement("p");
+  p1.className = "textitem";
+  p1.textContent = taskName;
 
-  var h11 = document.createElement("h1")
-  h11.className = "textitem"
-  h11.textContent = document.getElementById("tareaDate").value + " a las " + document.getElementById("tareaTime").value
+  var h11 = document.createElement("h1");
+  h11.className = "textitem";
+  h11.textContent = taskDate + " a las " + taskTime;
 
-  div2.appendChild(p1)
-  div2.appendChild(h11)
-  divPrincipal.appendChild(div2)
+  div2.appendChild(p1);
+  div2.appendChild(h11);
+  divPrincipal.appendChild(div2);
 
-  var br1 = document.createElement("br")
+  var br1 = document.createElement("br");
 
-  navToAppend.appendChild(br1)
+  navToAppend.appendChild(br1);
+
+  //export a cookie
+  tasksMade.push(new Task(taskID, taskName, taskDate, taskTime));
+  document.cookie = "tasksMade=" + tasksMade;
+  document.cookie = "lastTaskID=" + taskID;
 }
